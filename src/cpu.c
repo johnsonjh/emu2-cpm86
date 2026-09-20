86bc628d19645bf95c2e648d9a2e2cdc791ba76d
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-09-19 21:19:01 -0400

    Fix for DDT86 and SID86 debuggers
    GitHub Issue #22
    
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- src/cpu.c
+++ src/cpu.c
@@ -475,7 +475,9 @@ static void do_retf(void)
 static void trap_1(void)
 {
     next_instruction();
-    interrupt(1);
+    // INT/IRET/POPF clear TF as they execute; the 8086 suppresses the trap then.
+    if(TF)
+        interrupt(1);
 }
 
 static void do_popf(void)
